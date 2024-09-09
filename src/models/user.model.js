91a1241279accc6da809c53fa4b0c71225b8c1b9
devansh07mehta <devansh1622mht@gmail.com>
2024-09-09 18:40:22 +0530

    added user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -76,7 +76,6 @@ userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-      email: this.email,
     },
     process.env.REFRESH_TOKEN_SECRET,
     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
