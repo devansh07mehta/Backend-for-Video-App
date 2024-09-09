241584bde3a836fc515ac0f2945629309833b5ee
devansh07mehta <devansh1622mht@gmail.com>
2024-09-09 23:15:55 +0530

    added routes & controller for user

---
+++ src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { registerUser } from "../controllers/user.controllers.js";
+import { Router } from "express";
+
+const router = Router();
+router.route("/register").post(registerUser);
+
+export default router;
